5281c5bc148c691a7e64c84bb93fc6bc13bb1bb2
Jake Baker <SEP.Baker@fronius.com>
2025-02-12 15:03:41 -0500

    Improved day 3

--- Day03.cs
+++ Day03.cs
@@ -28,26 +28,33 @@ public class Day03
     (r1 * (~r1 & zed)).Should().Be(expected);
   }
 
-  // [Theory]
-  // [InlineData("Day03.Sample", 230)]
-  // [InlineData("Day03", 0)]
-  // public void Part2(string file, long expected)
-  // {
-  //   var grid = Convert(AoCLoader.LoadLines(file));
-  //   List<char> mostCommons = [];
-  //   var rows = grid.Max(it => it.Key.Y);
-  //   var cols = grid.Max(it => it.Key.X) + 1;
-  //   foreach(var col in MiscUtils.LongRange(0, cols))
-  //   {
-  //     var ones = grid.Where(it => it.Key.X == col).Count(it => it.Value == '1');
-  //     mostCommons.Add(ones > rows / 2 ? '1' : '0');
-  //   }
-
-  //   var ogr = grid.ToHash
-
-  //   long zed = MathUtils.LongPow(2, cols) - 1;
-  //   (r1 * (~r1 & zed)).Should().Be(expected);
-  // }
+  [Theory]
+  [InlineData("Day03.Sample", 230)]
+  [InlineData("Day03", 0)]
+  public void Part2(string file, long expected)
+  {
+    var grid = Convert(AoCLoader.LoadLines(file));
+    var rows = grid.Count;
+    var cols = grid[0].Length;
+    string zeds = "";
+    foreach(var col in Enumerable.Range(0, cols))
+    {
+      var ones = grid.Count(it => it[col] == '1');
+      zeds += (ones > rows / 2) ? '1' : '0';
+    }
+    
+    List<string> ogrs = [..grid];
+    List<string> csrs = [..grid];
+    foreach(var col in Enumerable.Range(0, cols))
+    {
+      var o = zeds[col];
+      var c = o == '1' ? '0': '1';
+      if (ogrs.Count > 1) ogrs = ogrs.Where(it => it[col] == o).ToList();
+      if (csrs.Count > 1) csrs = csrs.Where(it => it[col] == c).ToList();
+    }
+    (System.Convert.ToInt64(ogrs[0]) * System.Convert.ToInt64(csrs[0]))
+      .Should().Be(expected);
+  }
 
 
 
