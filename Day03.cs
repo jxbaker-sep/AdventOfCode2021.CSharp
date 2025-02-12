08f8f758ddcf9f23a36558d3437f776366aed164
Jake Baker <SEP.Baker@fronius.com>
2025-02-12 14:59:02 -0500

    Improved day 3

--- Day03.cs
+++ Day03.cs
@@ -17,21 +17,42 @@ public class Day03
   {
     var grid = Convert(AoCLoader.LoadLines(file));
     long r1 = 0;
-    var rows = grid.Max(it => it.Key.Y);
-    var cols = grid.Max(it => it.Key.X) + 1;
-    foreach(var col in MiscUtils.LongRange(0, cols))
+    var rows = grid.Count;
+    var cols = grid[0].Length;
+    foreach(var col in Enumerable.Range(0, cols))
     {
-      var ones = grid.Where(it => it.Key.X == col).Count(it => it.Value == '1');
+      var ones = grid.Count(it => it[col] == '1');
       r1 = (r1 << 1) + ((ones > rows / 2) ? 1 : 0);
     }
     long zed = MathUtils.LongPow(2, cols) - 1;
     (r1 * (~r1 & zed)).Should().Be(expected);
   }
 
+  // [Theory]
+  // [InlineData("Day03.Sample", 230)]
+  // [InlineData("Day03", 0)]
+  // public void Part2(string file, long expected)
+  // {
+  //   var grid = Convert(AoCLoader.LoadLines(file));
+  //   List<char> mostCommons = [];
+  //   var rows = grid.Max(it => it.Key.Y);
+  //   var cols = grid.Max(it => it.Key.X) + 1;
+  //   foreach(var col in MiscUtils.LongRange(0, cols))
+  //   {
+  //     var ones = grid.Where(it => it.Key.X == col).Count(it => it.Value == '1');
+  //     mostCommons.Add(ones > rows / 2 ? '1' : '0');
+  //   }
+
+  //   var ogr = grid.ToHash
+
+  //   long zed = MathUtils.LongPow(2, cols) - 1;
+  //   (r1 * (~r1 & zed)).Should().Be(expected);
+  // }
 
 
-  private static Dictionary<Point, char> Convert(List<string> list)
+
+  private static List<string> Convert(List<string> list)
   {
-    return list.Gridify();
+    return list;
   }
 }
