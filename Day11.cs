1cf41a33c85309bff89a1dcb5ec55927fd441244
Jake Baker <SEP.Baker@fronius.com>
2025-03-17 11:54:25 -0400

    day11.2

--- Day11.cs
+++ Day11.cs
@@ -9,15 +9,16 @@ public class Day11
 {
   
   [Theory]
-  [InlineData("Day11.Sample", 1656)]
-  [InlineData("Day11", 1749)]
-  public void Part1(string file, long expected)
+  [InlineData("Day11.Sample", 1656, 195)]
+  [InlineData("Day11", 1749, 285)]
+  public void Part1(string file, long expected, long expected2)
   {
     var grid = Convert(AoCLoader.LoadLines(file));
 
     long totalFlashes = 0;
+    long? firstAllFlashed = null;
 
-    foreach(var _ in Enumerable.Range(0, 100))
+    for(long i = 1; firstAllFlashed == null; i++)
     {
       grid = grid.ToDictionary(it => it.Key, it => it.Value + 1);
 
@@ -26,16 +27,18 @@ public class Day11
       {
         var current = grid.First(it => it.Value > 9 && !closed.Contains(it.Key));
         closed.Add(current.Key);
-        totalFlashes += 1;
+        if (i <= 100) totalFlashes += 1;
         foreach(var n in current.Key.CompassRoseNeighbors())
         {
           if (grid.ContainsKey(n)) grid[n] += 1;
         }
       }
       foreach(var pt in closed) grid[pt] = 0;
+      if (closed.Count == grid.Count && firstAllFlashed == null) firstAllFlashed = i;
     }
 
     totalFlashes.Should().Be(expected);
+    firstAllFlashed.Should().Be(expected2);
   }
 
   private static Dictionary<Point, long> Convert(List<string> input)
