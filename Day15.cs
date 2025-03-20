b8633d5ffb6a942012966c84fdf9eef798b173b2
Jake Baker <SEP.Baker@fronius.com>
2025-03-20 13:44:38 -0400

    day15.refactor

--- Day15.cs
+++ Day15.cs
@@ -34,9 +34,9 @@ public class Day15
 
     foreach (var y in Enumerable.Range(0, 5))
     {
-      foreach(var x in Enumerable.Range(0, 5))
+      foreach (var x in Enumerable.Range(0, 5))
       {
-        foreach(var (Key, Value) in grid.Items())
+        foreach (var (Key, Value) in grid.Items())
         {
           long value = Value + y + x;
           if (value > 9) value -= 9;
@@ -51,25 +51,23 @@ public class Day15
   static long FindLeastRiskyPath(Grid<long> grid)
   {
     var goal = new Point(grid.Width - 1, grid.Height - 1);
-    Dictionary<Point, long> closed = [];
-    closed[Point.Zero] = 0;
+    HashSet<Point> closed = [Point.Zero];
 
-    Queue<(Point Point, long Risk)> open = [];
+    PriorityQueue<(Point Point, long Risk)> open = new(it => it.Risk);
     open.Enqueue((Point.Zero, 0));
 
     while (open.TryDequeue(out var current))
     {
-      if (closed.TryGetValue(current.Point, out var preexisting) && preexisting < current.Risk) continue;
-      foreach(var n in current.Point.CardinalNeighbors()) {
-        if (grid.TryGetValue(n, out var risk)) {
-          if (closed.TryGetValue(n, out var existing) && existing <= current.Risk + risk) continue;
-          closed[n] = current.Risk + risk;
-          open.Enqueue((n, current.Risk + risk));
-        }
+      foreach (var n in current.Point.CardinalNeighbors())
+      {
+        if (!grid.TryGetValue(n, out var risk)) continue;
+        if (!closed.Add(n)) continue;
+        if (n == goal) return current.Risk + risk;
+        open.Enqueue((n, current.Risk + risk));
       }
     }
 
-    return closed[goal];
+    throw new ApplicationException();
   }
 
   private static Grid<long> Convert(List<string> input)
