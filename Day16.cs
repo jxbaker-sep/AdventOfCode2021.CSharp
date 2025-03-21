24bfb9969385f564660ffa5e36cd5ee2278f8f32
Jake Baker <SEP.Baker@fronius.com>
2025-03-21 15:46:48 -0400

    day16.2

--- Day16.cs
+++ Day16.cs
@@ -13,43 +13,73 @@ namespace AdventOfCode2021.CSharp;
 public class Day16
 {
   [Theory]
-  [InlineData("Day16", 963)]
-  public void Part1(string file, long expected)
+  [InlineData("Day16", 963, 1549026292886)]
+  public void Part1(string file, long expected, long expected2)
   {
     var input = AoCLoader.LoadLines(file).Single();
 
     var (packet, _) = ParsePacket(ToBinary(input));
     SumVersions(packet).Should().Be(expected);
+    Value(packet).Should().Be(expected2);
   }
 
   [Theory]
   [InlineData("D2FE28", 2021)]
+  public void SanityLiteral(string input, long expected)
+  {
+    var s = ToBinary(input);
+    var (packet, _) = ParsePacket(s);
+    packet.Literal.Should().Be(expected);
+  }
+
+  [Theory]
+  [InlineData("38006F45291200", 9)]
+  [InlineData("EE00D40C823060", 14)]
+  [InlineData("8A004A801A8002F478", 16)]
+  [InlineData("620080001611562C8802118E34", 12)]
+  [InlineData("C0015000016115A2E0802F182340", 23)]
+  [InlineData("A0016C880162017C3686B18A3D4780", 31)]
+  public void SanityOperators(string input, long expected)
+  {
+    var s = ToBinary(input);
+    var (packet, _) = ParsePacket(s);
+    SumVersions(packet).Should().Be(expected);
+  }
+
+  [Theory]
+  [InlineData("C200B40A82", 3)]
+  [InlineData("04005AC33890", 54)]
+  [InlineData("880086C3E88112", 7)]
+  [InlineData("CE00C43D881120", 9)]
+  [InlineData("D8005AC2A8F0", 1)]
+  [InlineData("F600BC2D8F", 0)]
+  [InlineData("9C005AC2F8F0", 0)]
+  [InlineData("9C0141080250320F1802104A08", 1)]
   public void SanityValues(string input, long expected)
   {
     var s = ToBinary(input);
     var (packet, _) = ParsePacket(s);
-    packet.Value.Should().Be(expected);
+    Value(packet).Should().Be(expected);
   }
 
-  [Theory]
-  [InlineData("38006F45291200", 9, 30)]
-  [InlineData("EE00D40C823060", 14, 6)]
-  [InlineData("8A004A801A8002F478", 16, -1)]
-  [InlineData("620080001611562C8802118E34", 12, -1)]
-  [InlineData("C0015000016115A2E0802F182340", 23, -1)]
-  [InlineData("A0016C880162017C3686B18A3D4780", 31, -1)]
-  public void SanityOperators(string input, long expectedVersionSum, long expectedValueSum)
-  {
-    var s = ToBinary(input);
-    var (packet, _) = ParsePacket(s);
-    SumVersions(packet).Should().Be(expectedVersionSum);
-    if (expectedValueSum != -1) SumValue(packet).Should().Be(expectedValueSum);
-  }
-
-  record Packet(long Version, long ID, long Value, List<Packet> Subpackets);
+  record Packet(long Version, long ID, long Literal, List<Packet> Subpackets);
 
   static long SumVersions(Packet packet) => packet.Version + packet.Subpackets.Select(SumVersions).Sum();
-  static long SumValue(Packet packet) => packet.Value + packet.Subpackets.Select(SumValue).Sum();
+
+  static long Value(Packet packet)
+  {
+    return packet.ID switch {
+      0 => packet.Subpackets.Select(Value).Sum(),
+      1 => packet.Subpackets.Select(Value).Product(),
+      2 => packet.Subpackets.Select(Value).Min(),
+      3 => packet.Subpackets.Select(Value).Max(),
+      4 => packet.Literal,
+      5 => Value(packet.Subpackets[0]) > Value(packet.Subpackets[1]) ? 1 : 0,
+      6 => Value(packet.Subpackets[0]) < Value(packet.Subpackets[1]) ? 1 : 0,
+      7 => Value(packet.Subpackets[0]) == Value(packet.Subpackets[1]) ? 1 : 0,
+      _ => throw new ApplicationException()
+    };
+  }
 
   static (Packet Packet, string Remainder) ParsePacket(string s)
   {
