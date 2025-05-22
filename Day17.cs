372daa442456a95e4d94c8208df2a3fe9795eba6
Jake Baker <SEP.Baker@fronius.com>
2025-05-22 12:53:17 -0400

    day17.2

--- Day17.cs
+++ Day17.cs
@@ -57,6 +57,65 @@ public class Day17
     maxY.Should().Be(expected);
   }
 
+  [Theory]
+  [InlineData("Day17.Sample", 112)]
+  [InlineData("Day17", 4748)]
+  public void Part2(string file, long expected)
+  {
+    var targetArea = Parse(AoCLoader.LoadLines(file).Single());
+
+    // times when X intersects target area.
+    Dictionary<long, List<long>> t2xs = [];
+    Dictionary<long, long> infinities = [];
+
+    for (var startdx = targetArea.X2; startdx > 0; startdx--)
+    {
+      long x = 0;
+      var t = 1;
+      for (var dx = startdx; dx > 0 && x <= targetArea.X2; dx--, t++)
+      {
+        x += dx;
+        if (targetArea.X1 <= x && x <= targetArea.X2) {
+          t2xs[t] = [.. (t2xs.GetValueOrDefault(t) ?? []), startdx];
+          if (dx == 1) {
+            infinities.Should().NotContainKey(t + 1);
+            infinities[t + 1] = startdx;
+          }
+        }
+      }
+    }
+
+    HashSet<(long,long)> count = [];
+
+    for (var startdy = targetArea.Y2; startdy <= -targetArea.Y2 ; startdy++)
+    {
+      long y = 0;
+      long t = 1; 
+      long dy = startdy;
+      if (startdy > 0) {
+        dy = -(startdy + 1); // falling
+        t = startdy * 2 + 2; // when velocity crosses 0 again
+      }
+      while (y >= targetArea.Y2)
+      {
+        y += dy;
+        if (targetArea.Y1 >= y && y >= targetArea.Y2) {
+          foreach (var x in t2xs.GetValueOrDefault(t) ?? []) {
+            count.Add((startdy, x));
+          }
+          foreach (var x in infinities.Where(tx => tx.Key <= t))
+          {
+            count.Add((startdy, x.Value));
+          }
+        }
+        dy -= 1;
+        t += 1;
+      }
+    }
+
+    count.Count.Should().Be((int)expected);
+  }
+
   public record TargetArea(long X1, long X2, long Y1, long Y2);
 
   private static TargetArea Parse(string line)
